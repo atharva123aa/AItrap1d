6c558652c06b97f40f1fd513a3372081cc28ab10
Atharva Awasthi <atharva123aa@gmail.com>
2026-06-18 23:08:17 +0530

    i remembered i had to commit
    fixed  a lot of bugs and use claude for 4 5

--- app.py
+++ app.py
@@ -1,56 +1,71 @@
 from textual.app import App
-from textual.widgets import Static,Input, Richlog
+from textual.widgets import Static,Input, RichLog
 from textual.containers import Horizontal
 class AITrap1dApp(App):
 
     CSS_PATH="style.css"
+    ENABLE_COMMAND_PALETTE=False
     #9 56
     def on_mount(self):
         self.value=""
-        self.log  ="[[ CONNECTION LOST ]]\n\nLocation: INSIDE AIRL(AI IN REAL LIFE)\n\nAccess Level:0\n\n" \
-        "You are a software enginer/nSomething went wrong during deployement\n\n\AIRL has detected a foe ......YOUUUU\n\n" \
+        self.intro ="[[ CONNECTION LOST ]]\n\nLocation: INSIDE AIRL(AI IN REAL LIFE)\n\nAccess Level:0\n\n" \
+        "You are a software enginer/nSomething went wrong during deployement\n\nAIRL has detected a foe ......YOUUUU\n\n" \
         "Encryptioned ID:A=>1 |18-0-1\n\nprove you are human before you lock out \n\nType help\n\n"
         self.c_level=1
         self.h_used=0
         self.f_opened=0
         self.memory=0
         self.truth=False
+        terminal=self.query_one("#history",RichLog)
+        terminal.write(self.intro)
+        self.query_one("#cmd",Input).focus()
     #Hey i am not sur now so i could eete some or add as the story is not going to be that vast
     def compose(self):
-        self.input=Input(placeholder="enter cmd...")
-        yield Static ("[[ CONNECTION LOCKED]]\n\Location: INSIDE AIRL (AI IN REAL LIFE))\n\nAccess Level:0\n\n" \
-                      "You are a software engineer \nSomething went wrong during deployement\n\n\AIRL has detected a foe ......YOUUUU\n\n" \
-                      "Encryptioned ID:A=> 1 |18-9-1\n\bprove you are human before you lock out \n\n",id="history",markup=False)
+     
+        yield RichLog(id="history",markup=False)
         with Horizontal(id="input"):
-            yield Richlog(">" ,id="prompt")
-            yield self.input
+            yield Static (">" ,id="prompt")
+            yield Input(placeholder="enter cmd...",id="cmd")
+            
 #work on richlog appliance over static-:}
-    def _on_key(self,event):
+   
+    def action_quit(self):
+        self.exit(0)
+    BINDINGS=[("ctrl+c","quit","Quit")]
+    def  on_key(self,event):
+       
         if event.key== "up":
 
-            self.input.value== self.value
-            self.input.cursor_position=len(self.value)
+            inp=self.query_one("#cmd", Input)
+            inp.value=self.value
+            inp.cursor_position=len(self.value)
     
-    def on_input_submit(self,event):
+    def on_input_submitted(self,event):
         self.value=event.value.strip().lower()
-        value_list=self.value.split("",1)
+        value_list=self.value.split(" ",1)
         command = value_list[0]
         argument = value_list[1] if len(value_list) > 1 else None
 
         command  =value_list[0]
         argument =value_list[1] if len(value_list) > 1 else None
         # i wanna to make it for the normal game but the terminal part is easy and so i did this but i will try to make other version of it not of terminal
-        terminal=self.query_one("#history",Richlog)
+        terminal=self.query_one("#history",RichLog)
+       
+    
+
+       
         if self.value=="clear":
             terminal.clear()
+            return
         elif self.value=="leave":
-            self.leave()
+            self.exit(0)
             return
         else:
-            terminal.write(f"> {self.value}")
-            terminal.write("Unknown text,type help\n")
-        self.input.value=""
 
+            terminal.write(f"> {self.value}")
+             # i aint lowk sure that this will be helpfuls
+        self.query_one("#cmd",Input).value=""
+        self.query_one("#cmd",Input).focus()
 
         if self.value=="help" and self.c_level< 4:
             terminal.write("> help")
@@ -58,11 +73,11 @@ class AITrap1dApp(App):
         elif command=="scan" and len(value_list)!= 2 and self.c_level==1:
             terminal.write(f">{self.value}")   
             terminal.write("fistly write id \n")      
-        elif command=="scan"and value_list[1]=="airl" and self.c_level==1:
+        elif command=="scan"and argument=="airl" and self.c_level==1:
             terminal.write(f">{self.value}")
             terminal.write("wrong id decrypt the msg first\n")
-        elif  command=="scan" and value_list[1]=="airl" and self.c_level==1:
-            terminal.write("[[ACCESS LEVEL 1 UUNLOCK]]")
+        elif  command=="scan" and argument=="airl" and self.c_level==1:
+            terminal.write("[[ACCESS LEVEL 1 UNLOCK]]")
             terminal.write("Level 2:\nexplore the system\n try to connect\n type ls\n")
             self.h_used=0
             self.c_level=2
@@ -71,6 +86,7 @@ class AITrap1dApp(App):
             if self.h_used==0:
                 terminal.write(f">{self.value}  1")
                 terminal.write("[AIRL]: TRY CONVERTING NUM TO LETTER \n")
+                self.h_used=1
             elif self.h_used==1:
                 terminal.write(f"> {self.value} 2")
                 terminal.write("[AIRL]: EXAMPLE:\n A=1\n B=2\n D=4")
@@ -90,7 +106,15 @@ class AITrap1dApp(App):
                 terminal.write("you must tell a file \n") 
             elif argument=="users":
                 terminal.write(f">{self.value}")
-                terminal.write("===USERS ===\nroot\ndev\nguest \n\nNote"\nroot is the main acc.\n" )
+                terminal.write(
+                               "===USERS===\n"
+                               "root\n"
+                               "dev\n"
+                               "guest \n\n"
+                               "Note:\n"
+                               "root is the main acc.\n" 
+                )
+                
                 self.f_opened+=1
             elif argument=="logs":
                 terminal.write(f"> {self.value}")
@@ -124,7 +148,7 @@ class AITrap1dApp(App):
             else:
                  terminal.write(f">{self.value}")
                  terminal.write( "wrong pass\n access deny \n")
-        elif self.value==="hint" and self.c_level==2:
+        elif self.value=="hint" and self.c_level==2:
             if self.h_used==0:
                  terminal.write(f">{self.value} 1")
                  terminal.write("[AIRL]: FOCUS ON ROOT ACC. \n something important happened in a specific year \n")
@@ -139,24 +163,19 @@ class AITrap1dApp(App):
                  self.h_used=3
             elif self.h_used== 3:
                  terminal.write(f">{self.value} ")
-                 terminal.write("[AIRL]:SHAME ON YOU NO MOR HINTS \n")
-
+                 terminal.write("[AIRL]:SHAME ON YOU NO MORE HINTS \n")
             
-
-             
-
-           
-
-
-            
-                               
+        elif command=="solve" and self.c_level not in [3,3.5]:
+         terminal.write(f"> {self.value}")
+         terminal.write("nothing to solve here yet")
+        
                 
 
-
-
-
-
-    
-
-        
-                      
+            
+        elif self.value == "clear":
+            self.history=""
+if __name__ == "__main__":
+    AITrap1dApp().run()
+            
+            
+            
